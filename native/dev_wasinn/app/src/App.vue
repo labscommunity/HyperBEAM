aa9e216793afd73b2ca0904fb424dac91213048e
Saam Tehrani <samanrtehrani@gmail.com>
2025-06-12 16:23:09 -0600

    init vue/tailwind

---
+++ native/dev_wasinn/app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+  
+    <div class="bg-yellow-500">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
