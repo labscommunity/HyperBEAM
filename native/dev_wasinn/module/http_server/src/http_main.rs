dfbfd1bf936ca6c6500a4db6034ac52c58de4325
Saam Tehrani <samanrtehrani@gmail.com>
2025-06-12 16:52:48 -0500

    add direct onnx chat completions server

--- native/dev_wasinn/module/http_server/src/http_main.rs
+++
@@ -1,9 +0,0 @@
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    println!("🚀 Starting server and loading module...");
-    
-    let wasm_module_path = "../inferencer/target/wasm32-wasip1/release/ncl_ml.wasm".to_string();
-    
-    http_server::start_server(3000, wasm_module_path).await
-}
